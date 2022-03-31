1ebf8a67e3b4d759529cf139a01b97816a728a36
ArAnGor <gorbart01@mail.ru>
2022-03-31 16:27:03 +0300

    add header

---
+++ modules/task_1/gorbachev_a_character_mismatch/character_mismatch.h
@@ -0,0 +1,11 @@
+// Copyright 2022 Gorbachev Artem
+#ifndef MODULES_TASK_1_GORBACHEV_A_CHARACTER_MISMATCH_GORBACHEV_A_CHARACTER_MISMATCH_H_
+#define MODULES_TASK_1_GORBACHEV_A_CHARACTER_MISMATCH_GORBACHEV_A_CHARACTER_MISMATCH_H_
+
+#include <string>
+
+std::string getRandomString(int size);
+int parallelCountMismatch(const std::string str1, const std::string str2);
+int sequentialCountMismatch(const std::string str1, const std::string str2);
+
+#endif  // MODULES_TASK_1_GORBACHEV_A_CHARACTER_MISMATCH_GORBACHEV_A_CHARACTER_MISMATCH_H_
