847fe72c09b64acac457e35c4cfb6fd19dc067e2
ArAnGor <gorbart01@mail.ru>
2022-03-31 16:43:40 +0300

    add cpp

---
+++ modules/task_1/gorbachev_a_character_mismatch/character_mismatch.cpp
@@ -0,0 +1,18 @@
+// Copyright 2022 Gorbachev Artem
+#include <mpi.h>
+#include <string>
+#include "../../../modules/task_1/gorbachev_a_character_mismatch/character_mismatch.h"
+
+std::string getRandomString(int size) {
+  return "";
+}
+
+int parallelCountMismatch(const std::string str1, const std::string str2)
+{
+  return 0;
+}
+
+int sequentialCountMismatch(const std::string str1, const std::string str2)
+{
+  return 0;
+}
