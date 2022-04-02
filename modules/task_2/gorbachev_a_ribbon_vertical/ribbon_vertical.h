709d7b2eeb1eedbb4b2ebd03e00a7cc820132e03
ArAnGor <gorbart01@mail.ru>
2022-04-02 20:59:41 +0300

    fix spaces

--- modules/task_2/gorbachev_a_ribbon_vertical/ribbon_vertical.h
+++ modules/task_2/gorbachev_a_ribbon_vertical/ribbon_vertical.h
@@ -5,13 +5,13 @@
 #include <vector>
 
 std::vector<std::vector<int>>& getRandomMatrix(int m, int n);
-std::vector<std::vector<int> >& sequentialMultiplication(
-  const std::vector<std::vector<int> >& A,
-  const std::vector<std::vector<int> >& B,
+std::vector<std::vector<int>>& sequentialMultiplication(
+  const std::vector<std::vector<int>>& A,
+  const std::vector<std::vector<int>>& B,
   int m, int n, int k);
-std::vector<std::vector<int> >& parallelMultiplication(
-  const std::vector<std::vector<int> >& A,
-  const std::vector<std::vector<int> >& B,
+std::vector<std::vector<int>>& parallelMultiplication(
+  const std::vector<std::vector<int>>& A,
+  const std::vector<std::vector<int>>& B,
   int m, int n, int k);
 
 #endif  // MODULES_TASK_2_GORBACHEV_A_RIBBON_VERTICAL_RIBBON_VERTICAL_H_
