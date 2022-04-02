ccdde818b1d514866105a5a02e6fa9415c0e436f
ArAnGor <gorbart01@mail.ru>
2022-04-02 21:58:59 +0300

    add test

---
+++ modules/task_2/gorbachev_a_ribbon_vertical/main.cpp
@@ -0,0 +1,60 @@
+#include <gtest/gtest.h>
+#include "ribbon_vertical.h"
+#include <gtest-mpi-listener.hpp>
+
+TEST(Parallel_Operations_MPI, Sequential_Square) {
+  std::vector<std::vector<int>> A = {
+    { 1, 2 },
+    { 3, 4 }
+  };
+  std::vector<std::vector<int>> B = {
+    { 1, 2 },
+    { 1, 0 }
+  };
+  std::vector<std::vector<int>> C = {
+    { 3, 2 },
+    { 7, 6 }
+  };
+  std::vector<std::vector<int>> M = sequentialMultiplication(A, B, 2, 2, 2);
+  //printf("%d %d\n%d %d\n", M[0][0], M[0][1], M[1][0], M[1][1]);
+  ASSERT_EQ(M, C);
+}
+
+TEST(Parallel_Operations_MPI, Sequential_Rectangle) {
+  std::vector<std::vector<int>> A = {
+    { 1, 2, 3 },
+    { 4, 0, 5 },
+    { 6, 7, 8 }
+  };
+}
+
+TEST(Parallel_Operations_MPI, Parallel_Square) {
+
+}
+
+TEST(Parallel_Operations_MPI, Parallel_Rectangle) {
+
+}
+
+TEST(Parallel_Operations_MPI, Identity_Matrix) {
+
+}
+
+TEST(Parallel_Operations_MPI, Zero_Matrix) {
+
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  MPI_Init(&argc, &argv);
+
+  ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+  ::testing::TestEventListeners& listeners =
+    ::testing::UnitTest::GetInstance()->listeners();
+
+  listeners.Release(listeners.default_result_printer());
+  listeners.Release(listeners.default_xml_generator());
+
+  listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+  return RUN_ALL_TESTS();
+}
