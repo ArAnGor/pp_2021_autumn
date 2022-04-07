09fa5f9bfcb8d28f01bd8bdad0ef1003b0cb0030
ArAnGor <gorbart01@mail.ru>
2022-04-07 18:14:34 +0300

    add header

---
+++ modules/task_3/gorbachev_a_component_marking/component_marking.h
@@ -0,0 +1,9 @@
+// Copyright 2022 Gorbachev Artem
+#ifndef MODULES_TASK_3_GORBACHEV_A_COMPONENT_MARKING_COMPONENT_MARKING_H_
+#define MODULES_TASK_3_GORBACHEV_A_COMPONENT_MARKING_COMPONENT_MARKING_H_
+
+int* getRandomBinary(int x, int y);
+int sequentialMarking(int* image, int x, int y);
+int parallelMarking(int* image, int x, int y);
+
+#endif // MODULES_TASK_3_GORBACHEV_A_COMPONENT_MARKING_COMPONENT_MARKING_H_
