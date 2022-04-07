85bfc05412bcfc7bd002a83251a6ce738d1b4d02
ArAnGor <gorbart01@mail.ru>
2022-04-08 01:55:08 +0300

    add tests

--- modules/task_3/gorbachev_a_component_marking/main.cpp
+++ modules/task_3/gorbachev_a_component_marking/main.cpp
@@ -8,10 +8,6 @@ TEST(Parallel_Operations_MPI, Sequential_Simple) {
     1, 0,
     0, 1
   };
-  int expected[] = {
-    1, 0,
-    0, 2
-  };
   int res = sequentialMarking(image, 2, 2);
   for (int i = 0; i < 2; i++) {
     for (int j = 0; j < 2; j++)
@@ -19,7 +15,6 @@ TEST(Parallel_Operations_MPI, Sequential_Simple) {
     printf("\n");
   }
   ASSERT_EQ(res, 2);
-  //ASSERT_EQ(image, expected);
 }
 
 TEST(Parallel_Operations_MPI, Sequential_Collision) {
@@ -29,12 +24,6 @@ TEST(Parallel_Operations_MPI, Sequential_Collision) {
     0, 0, 0, 1, 0, 1,
     1, 1, 1, 1, 1, 1
   };
-  int expected[] = {
-    0, 1, 0, 2, 0, 2,
-    1, 1, 0, 2, 0, 2,
-    0, 0, 0, 2, 0, 2,
-    2, 2, 2, 2, 2, 2
-  };
   int res = sequentialMarking(image, 6, 4);
   for (int i = 0; i < 4; i++) {
     for (int j = 0; j < 6; j++)
@@ -42,7 +31,82 @@ TEST(Parallel_Operations_MPI, Sequential_Collision) {
     printf("\n");
   }
   ASSERT_EQ(res, 2);
-  //ASSERT_EQ(image, expected);
+}
+
+TEST(Parallel_Operations_MPI, Parallel_Random) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  int *sImage, *pImage;
+  const int x = 10, y = 10;
+
+  if (rank == 0) {
+    pImage = getRandomBinary(x, y);
+    int size = x * y;
+    sImage = new int[size];
+    for (int i = 0; i < size; i++)
+      sImage[i] = pImage[i];
+  }
+
+  int res = parallelMarking(pImage, x, y);
+
+  if (rank == 0) {
+    int exp = sequentialMarking(sImage, x, y);
+    for (int i = 0; i < y; i++) {
+      for (int j = 0; j < x; j++)
+        printf("%d ", pImage[i * x + j]);
+      printf("\n");
+    }
+    ASSERT_EQ(res, exp);
+  }
+}
+
+TEST(Parallel_Operations_MPI, Parallel_Empty) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  int sImage[16] = { 0 };
+  int pImage[16] = { 0 };
+  const int x = 4, y = 4;
+
+  int res = parallelMarking(pImage, x, y);
+
+  if (rank == 0) {
+    int exp = sequentialMarking(sImage, x, y);
+    for (int i = 0; i < y; i++) {
+      for (int j = 0; j < x; j++)
+        printf("%d ", pImage[i * x + j]);
+      printf("\n");
+    }
+    ASSERT_EQ(res, exp);
+    ASSERT_EQ(res, 0);
+  }
+}
+
+TEST(Parallel_Operations_MPI, Parallel_Filled) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  int *sImage, *pImage;
+  const int x = 4, y = 4;
+
+  if (rank == 0) {
+    int size = x * y;
+    sImage = new int[size];
+    pImage = new int[size];
+    for (int i = 0; i < size; i++)
+      sImage[i] = pImage[i] = 1;
+  }
+
+  int res = parallelMarking(pImage, x, y);
+
+  if (rank == 0) {
+    int exp = sequentialMarking(sImage, x, y);
+    for (int i = 0; i < y; i++) {
+      for (int j = 0; j < x; j++)
+        printf("%d ", pImage[i * x + j]);
+      printf("\n");
+    }
+    ASSERT_EQ(res, exp);
+    ASSERT_EQ(res, 1);
+  }
 }
 
 int main(int argc, char** argv) {
